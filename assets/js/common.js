d39c3ac95abfa9dcd4f12f64652df952eb35bc37
elenakyrychenko93 <kaletinsky-eo@gbksoft.net>
2017-06-10 12:31:52 +0300

    'init'

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,3 +1,25 @@
 ﻿$(document).ready(function () {
+    $('.slider').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        adaptiveHeight: true,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    dots: false
+                }
+            },
+            {
+                breakpoint: 500,
+                settings: {
+                    dots: false
+                }
+            }
+        ]
+    });
+
 });
 
